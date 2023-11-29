eadcaceb40fd1f1cce250fc30b1be3b091d79d6b
Kalle Hahtela <khahtela@gmail.com>
2023-11-29 10:05:29 +0200

    added a google maps API with location and marker to showcase it

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function showMenu() {
-    document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('hide');
-    document.querySelector('.close').classList.toggle('show');
+  document.querySelector(".navigation").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("hide");
+  document.querySelector(".close").classList.toggle("show");
+}
+
+function initMap() {
+    var location = {lat: 61.505423, lng: 23.713918};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
 }
