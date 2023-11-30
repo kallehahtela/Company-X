5ae1fe88b71e1fa11d513061550f6cf3c9cc2bff
Kalle Hahtela <khahtela@gmail.com>
2023-11-30 21:12:11 +0200

    added a explore tab option, which is in the making, added a input field which will place marker in the current address typed in the field.

--- script.js
+++ script.js
@@ -4,15 +4,43 @@ function showMenu() {
   document.querySelector(".close").classList.toggle("show");
 }
 
-function initMap() {
-    var location = {lat: 61.505423, lng: 23.713918};
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: location
-    });
+/*function initMap() {
+  var location = { lat: 61.505423, lng: 23.713918 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: location,
+  });
 
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-}
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}*/
+
+let map;
+        let geocoder;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 0, lng: 0 },
+                zoom: 8
+            });
+            geocoder = new google.maps.Geocoder();
+        }
+
+        function addMarker() {
+            const address = document.getElementById('addressInput').value;
+
+            geocoder.geocode({ 'address': address }, function (results, status) {
+                if (status == 'OK') {
+                    const location = results[0].geometry.location;
+                    const marker = new google.maps.Marker({
+                        map: map,
+                        position: location
+                    });
+                    map.setCenter(location);
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        }
